81d3b77bac41c3741913398f1e651eba9715fa1a
Gabriel Miklós <socketbind@gmail.com>
2019-03-03 21:12:10 +0100

    Very minor cleanup.

---
+++ public/javascripts/common.js
@@ -0,0 +1,33 @@
+function toggleFullScreen() {
+    if (!document.fullscreenElement) {
+        document.documentElement.requestFullscreen();
+    } else {
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        }
+    }
+}
+
+function enableFullscreenToggle() {
+    document.body.addEventListener('click', () => toggleFullScreen());
+}
+
+// throttling code taken from MDN
+(function() {
+    var throttle = function(type, name, obj) {
+        obj = obj || window;
+        var running = false;
+        var func = function() {
+            if (running) { return; }
+            running = true;
+            requestAnimationFrame(function() {
+                obj.dispatchEvent(new CustomEvent(name));
+                running = false;
+            });
+        };
+        obj.addEventListener(type, func);
+    };
+
+    /* init - you can init any event */
+    throttle("resize", "optimizedResize");
+})();
