1b2a207fae9a0aa063bc9a37c1a268e2e0882b3a
Gabriel Miklós <socketbind@gmail.com>
2019-03-05 22:11:58 +0100

    Properly return photowall pics (upload time descending order).

--- routes/photowall.js
+++ routes/photowall.js
@@ -4,31 +4,35 @@ const path = require('path');
 const fs = require('fs');
 const uuid = require('uuid/v1');
 
-const { Hub, sseHub } = require('@toverux/expresse');
+const {Hub, sseHub} = require('@toverux/expresse');
 
 const hub = new Hub();
 
 /* GET home page. */
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
     fs.readdir(res.app.locals.filesDir, (err, files) => {
         if (err) {
             return res.status(500).send(err);
         }
 
-        const actualFiles = files.filter(file =>
-            fs.lstatSync(path.join(res.app.locals.filesDir, file)).isFile()
-        );
+        const actualFiles = files
+            .map(file => ({
+                name: file, stat: fs.lstatSync(path.join(res.app.locals.filesDir, file))
+            }))
+            .filter(file => file.stat.isFile())
+            .sort((a, b) => b.stat.ctimeMs - a.stat.ctimeMs)
+            .map(file => file.name);
 
-        res.render('photowall/index', { pageClass: 'page-photowall-index', files: actualFiles });
+        res.render('photowall/index', {pageClass: 'page-photowall-index', files: actualFiles});
     });
 });
 
-router.get('/upload/events', sseHub({ hub }), (req, res) => {
+router.get('/upload/events', sseHub({hub}), (req, res) => {
     res.sse.event('welcome', 'Welcome!');
 });
 
 router.get('/upload', (req, res) => {
-    res.render('photowall/upload', { pageClass: 'page-photowall-upload page-upload' });
+    res.render('photowall/upload', {pageClass: 'page-photowall-upload page-upload'});
 });
 
 router.post('/upload', (req, res) => {
